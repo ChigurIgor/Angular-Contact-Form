6e5c957be0c7283c4b82911dd68a09b573b078f8
IGOR CHIGUR <chigur.ig@gmail.com>
2019-02-05 15:52:06 +0200

    Home Work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular03022019';
+  contacts: Contact[] = [];
+  currName = '';
+  currEmail = '';
+  currPhone = '';
+  currAddress = '';
+  currContact: Contact|null = null;
+  currIndex: number = -1;
+
+  // addContact() {
+  //   this.contacts.push({
+  //     name: this.currName,
+  //     email: this.currEmail,
+  //     phone: this.currPhone,
+  //     address: this.currAddress
+  //   });
+  //   this.currName = '';
+  //   this.currEmail = '';
+  //   this.currPhone = '';
+  //   this.currAddress = '';
+  // }
+
+
+  addContact(contact) {
+    // console.log("THis from Futher: "+contact);
+      this.contacts.push(contact);
+  }
+
+  remove(indx: number): void {
+    this.contacts.splice(indx, 1);
+    if(indx === this.currIndex) {
+      this.currContact = null;
+    }
+  }
+
+  selectCurrent(contact: Contact, indx: number) {
+this.currContact = contact;
+this.currIndex = indx;
+
+  }
+
+
+}
+
+type Contact = {
+  name: string,
+  email: string,
+  phone: string,
+  address: string
 }

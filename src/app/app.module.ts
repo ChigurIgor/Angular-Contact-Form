c5b57c1d799747e4d9d6d51921f19813ca70a662
IGOR CHIGUR <chigur.ig@gmail.com>
2019-02-09 11:17:37 +0200

    Home work Add components description and list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import { MyInputComponent } from './my-input/my-input.component';
+import { ListComponent } from './list/list.component';
+import { DescriptionComponent } from './description/description.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyInputComponent
+    MyInputComponent,
+    ListComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule,
